d3627172ad4d4416c38901e4cc16e7e54597385e
mk01-v <prizma5150@gmail.com>
2019-11-13 14:31:56 +0700

    добавлен класс group в метод modify_group

--- fixture/group.py
+++ fixture/group.py
@@ -26,19 +26,19 @@ class GroupHelper:
         wd.find_element_by_name("submit").click()
         self.return_to_group_page()
 
-    def modif_group(self):
+    def modify_group(self, group):
         wd = self.app.wd
         self.open_group_page()
         wd.find_element_by_name("selected[]").click()
         wd.find_element_by_name("edit").click()
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send.keys(hhh)
+        wd.find_element_by_name("group_name").send_keys(group.name)
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send.keys(hhh)
+        wd.find_element_by_name("group_header").send_keys(group.name)
         wd.find_element_by_name("update").click()
-        wd.find_element_by_link_text("groups").click()
+        self.return_to_group_page()
 
     def delete_first_group(self):
         wd = self.app.wd
