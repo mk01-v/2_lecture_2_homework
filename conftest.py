558e76c345ec7c82b06ff8300ccd2aae12756b35
mk01-v <prizma5150@gmail.com>
2019-11-14 15:24:35 +0700

    Изменение фикстуры

--- conftest.py
+++ conftest.py
@@ -1,15 +1,12 @@
 from fixture.application import Application
 import pytest
 
-#@pytest.fixture(scope = 'session')
-#@pytest.fixture()
-#def ocb(request):
-#    fixture = Open_close_browser()
-#    request.addfinalizer(fixture.exit_browser)
-#    return fixture
-
 @pytest.fixture(scope = 'session')
 def app(request):
     fixture = Application()
-    request.addfinalizer(fixture.destroy)
+    fixture.session.login(username="admin", password="secret")
+    def fin():
+        fixture.session.logout()
+        fixture.destroy()
+    request.addfinalizer(fin)
     return fixture
