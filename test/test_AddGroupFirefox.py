02f060dd5a8efb70a824b4d4517c30bf22e66435
mk01-v <prizma5150@gmail.com>
2019-11-14 15:24:59 +0700

    удаление лишних строк

--- test/test_AddGroupFirefox.py
+++ test/test_AddGroupFirefox.py
@@ -3,12 +3,12 @@ from model.group import group
 
 
 def test_add_group(app):
-    app.session.login(username="admin", password="secret")
+
     app.group.create(group(name="group_name_cool", header="group_header_cool_logo"))
-    app.session.logout()
+
 
 def test_add_empty_group(app):
-    app.session.login(username="admin", password="secret")
+
     app.group.create(group(name="", header=""))
-    app.session.logout()
+
 
