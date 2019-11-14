9e8aa6ac4276945e8358ccccd6cf9f2f23e0218f
mk01-v <prizma5150@gmail.com>
2019-11-14 17:37:39 +0700

    Создание группы, если колво групп = 0.

--- test/test_delgroup.py
+++ test/test_delgroup.py
@@ -1,11 +1,12 @@
 #удаление группы.
-
+from model.group import Group
 
 def test_delete_first_group(app):
-
+    if app.group.count() == 0:
+        app.group.create(Group(name="test", header="qweqwe"))
     app.group.delete_first_group()
 
 
-def test_delete_first_group2(app):
 
-    app.group.delete_first_group()
+#def test_delete_first_group2(app):
+#    app.group.delete_first_group()
