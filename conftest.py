14a39928dfc9c01931881e0cb4063f8f454cc702
mk01-v <prizma5150@gmail.com>
2019-11-14 15:41:16 +0700

    Код проверки фикстуры перед запуском теста.

--- conftest.py
+++ conftest.py
@@ -1,10 +1,39 @@
 from fixture.application import Application
 import pytest
 
+
+#Для быстрого запуска - в рамках одной сессии.
+#@pytest.fixture(scope = 'session')
+#Для медленного запуска, сессия создается заново при инициализации теста.
+#pytest.fixture
+
+#@pytest.fixture(scope = 'session')
+#def app(request):
+#    fixture = Application()
+#    fixture.session.login(username="admin", password="secret")
+#    def fin():
+#        fixture.session.logout()
+#        fixture.destroy()
+#    request.addfinalizer(fin)
+#    return fixture
+
+fixture = None
+
 @pytest.fixture(scope = 'session')
 def app(request):
-    fixture = Application()
-    fixture.session.login(username="admin", password="secret")
+    global fixture
+    if fixture is None:
+        fixture = Application()
+        fixture.session.login(username="admin", password="secret")
+    else:
+        if not fixture.is_valid():
+            fixture = Application()
+            fixture.session.login(username="admin", password="secret")
+    return fixture
+
+# срабатывание всегда из-за autouse.
+@pytest.fixture(scope = "session", autouse=True)
+def stop(request):
     def fin():
         fixture.session.logout()
         fixture.destroy()
