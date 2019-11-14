9e45fc4585a47426fa7b19c9a9b79ef3166d7cea
mk01-v <prizma5150@gmail.com>
2019-11-14 13:29:09 +0700

    Статус: работает.

--- test/test_amodifygroup.py
+++ test/test_amodifygroup.py
@@ -4,5 +4,5 @@ from model.group import group
 
 def test_modifgroup(app):
     app.session.login(username="admin", password="secret")
-    app.group.modify_group(group(name="hhh1", header="hh2"))
+    app.group.modify_group(group(name="hhh1", header="hhh2"))
     app.session.logout()
