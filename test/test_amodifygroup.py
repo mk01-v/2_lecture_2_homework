c8911bb4390437870992a633e973d4ec4a4a3374
mk01-v <prizma5150@gmail.com>
2019-11-14 17:37:02 +0700

    ----

--- test/test_amodifygroup.py
+++ test/test_amodifygroup.py
@@ -1,13 +1,13 @@
 
-from model.group import group
+from model.group import Group
 
 
 def test_modifgroup_name(app):
 
-    app.group.modify_first_group(group(name="hhh1"))
+    app.group.modify_first_group(Group(name="hhh1"))
 
 
 def test_modifgroup_header(app):
 
-    app.group.modify_first_group(group(header="hhh41"))
+    app.group.modify_first_group(Group(header="hhh41"))
 
