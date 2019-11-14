3e25d58afb66af72f7382221ebce1eb7b6ff2359
mk01-v <prizma5150@gmail.com>
2019-11-14 15:25:19 +0700

    удаление лишних строк

--- test/test_delgroup.py
+++ test/test_delgroup.py
@@ -2,11 +2,10 @@
 
 
 def test_delete_first_group(app):
-    app.session.login(username="admin", password="secret")
+
     app.group.delete_first_group()
-    app.session.logout()
+
 
 def test_delete_first_group2(app):
-    app.session.login(username="admin", password="secret")
+
     app.group.delete_first_group()
-    app.session.logout()
