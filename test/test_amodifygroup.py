20e9261fa6d5fb2a18f44de11958379bdc5ba5be
mk01-v <prizma5150@gmail.com>
2019-11-14 15:25:15 +0700

    удаление лишних строк

--- test/test_amodifygroup.py
+++ test/test_amodifygroup.py
@@ -3,11 +3,11 @@ from model.group import group
 
 
 def test_modifgroup_name(app):
-    app.session.login(username="admin", password="secret")
+
     app.group.modify_first_group(group(name="hhh1"))
-    app.session.logout()
+
 
 def test_modifgroup_header(app):
-    app.session.login(username="admin", password="secret")
+
     app.group.modify_first_group(group(header="hhh41"))
-    app.session.logout()
+
