7b48088e66a49275abe19cc85d11ee58d77da86c
mk01-v <prizma5150@gmail.com>
2019-11-15 14:35:14 +0700

    Статус: работает. Удаление лишних комментариев.

--- fixture/group.py
+++ fixture/group.py
@@ -1,5 +1,4 @@
 
-
 class GroupHelper:
 
     def __init__(self, app):
@@ -14,22 +13,19 @@ class GroupHelper:
     def create(self, group):
         wd = self.app.wd
         self.open_group_page()
-        # init group creation
         wd.find_element_by_name("new").click()
-        # filling group
         self.fill_group_form(group)
-        # submit group creation
         wd.find_element_by_name("submit").click()
         self.return_to_group_page()
 
-    def modify(self, group):
-        wd = self.app.wd
-        self.open_group_page()
-        self.select_first_group()
-        wd.find_element_by_name("edit").click()
-        self.fill_group_form(group)
-        wd.find_element_by_name("update").click()
-        self.return_to_group_page()
+    #def modify(self, group):
+    #    wd = self.app.wd
+    #    self.open_group_page()
+    #    self.select_first_group()
+    #    wd.find_element_by_name("edit").click()
+    #    self.fill_group_form(group)
+    #    wd.find_element_by_name("update").click()
+    #    self.return_to_group_page()
 
     def modify_first_group(self, new_group_data):
         wd = self.app.wd
@@ -61,17 +57,13 @@ class GroupHelper:
     def delete_first_group(self):
         wd = self.app.wd
         self.open_group_page()
-        # select first group
         self.select_first_group()
-        # submit deletion
         wd.find_element_by_name("delete").click()
         self.return_to_group_page()
 
     def return_to_group_page(self):
         wd = self.app.wd
-        # return groups page
         wd.find_element_by_link_text("groups").click()
-        #wd.find_element_by_link_text("add new").click()
 
     def count(self):
         wd = self.app.wd
