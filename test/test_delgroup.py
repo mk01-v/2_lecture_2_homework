f5bf2ee45663294ff025999d8ddcf1519b56e983
mk01-v <prizma5150@gmail.com>
2019-11-15 13:00:52 +0700

    Для удаления 2 группы добавил 2-ой тест

--- test/test_delgroup.py
+++ test/test_delgroup.py
@@ -6,3 +6,8 @@ def test_delete_first_group(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test", header="qweqwe"))
     app.group.delete_first_group()
+
+def test_delete_first_group2(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="test", header="qweqwe"))
+    app.group.delete_first_group()
