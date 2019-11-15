38859fe0a1d7b89503c3eb9dd6a8fae10bb5664b
mk01-v <prizma5150@gmail.com>
2019-11-15 13:00:14 +0700

    Редактирование страницы - открытие теста удаления.

--- test/test_AddKontaktFirefox.py
+++ test/test_AddKontaktFirefox.py
@@ -2,7 +2,6 @@
 from model.kontakt import kontakt
 
 def test_add_kontakt_firefox(app):
-
     app.kontakt.create_kontakt(kontakt(username="First_name: Artur", middle_name="Middle_name: 123", last_name="Last_name: Piroshkov", nickname="Nickname: 123",
                             title="Title: addressbook", company="Company: OOO Privet", address="Address: Michurina 3", home="Home: 3",
                             mobile="Mobile: 9232847147", work="Work: None", fax="Fax: None", email="E-mail: 123@gmail.com",
@@ -12,7 +11,6 @@ def test_add_kontakt_firefox(app):
 
 
 def test_add_modif_kontakt(app):
-
     app.kontakt.modif_kontakt(kontakt(username="Artur123123", middle_name="Middle_name: 1230000", last_name="", nickname="",
                             title="", company="", address="", home="",
                             mobile="", work="", fax="", email="",
@@ -20,8 +18,5 @@ def test_add_modif_kontakt(app):
                             aday="10", amonth="December", ayear="2000", secondary_address2="", secondary_home2="",
                             secondary_notes=""))
 
-
-
-#def test_delete_kontakt(app):
-
-#    app.kontakt.delete_kontakt()
+def test_delete_kontakt(app):
+    app.kontakt.delete_kontakt()
