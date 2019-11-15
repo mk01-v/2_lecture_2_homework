e44d64dce36c90d113e7baa201039b6ba9ceff7d
mk01-v <prizma5150@gmail.com>
2019-11-15 13:14:58 +0700

    Распредение тестов по страницам

--- test/test_AddKontakt.py
+++ test/test_AddKontakt.py
@@ -10,13 +10,4 @@ def test_add_kontakt_firefox(app):
                             secondary_notes="Secondary-notes: None"))
 
 
-def test_add_modif_kontakt(app):
-    app.kontakt.modif_kontakt(kontakt(username="Artur123123", middle_name="Middle_name: 1230000", last_name="", nickname="",
-                            title="", company="", address="", home="",
-                            mobile="", work="", fax="", email="",
-                            email2="", email3="", homepage="", bday="10", bmonth="October", byear="1992",
-                            aday="10", amonth="December", ayear="2000", secondary_address2="", secondary_home2="",
-                            secondary_notes=""))
 
-def test_delete_kontakt(app):
-    app.kontakt.delete_kontakt()
