3ae614a0dc064db6637e53a05b2db7001d23aa30
mk01-v <prizma5150@gmail.com>
2019-11-15 17:08:30 +0700

    -----

--- test/test_5delgroup.py
+++ test/test_5delgroup.py
@@ -1,8 +1,6 @@
-#удаление группы.
 from model.group import Group
 
 def test_delete_first_group(app):
-    #Если кол-во групп отсутствует - создаем группу, затем удаляем.
     if app.group.count() == 0:
         app.group.create(Group(name="test", header="qweqwe"))
     app.group.delete_first_group()
