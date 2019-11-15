51adebf22e99297809b2e6aa5892dc5872b431a3
mk01-v <prizma5150@gmail.com>
2019-11-15 17:17:56 +0700

    Статус: работает. Добавлены условия для модификации групп.

--- test/test_3modifygroup.py
+++ test/test_3modifygroup.py
@@ -3,9 +3,12 @@ from model.group import Group
 
 
 def test_modifgroup_name(app):
-    app.group.modify_first_group(Group(name="hhh1"))
+    if app.group.count() == 0:
+        app.group.create(Group(name="000", header="000"))
+    app.group.modify_first_group(Group(name="hhh!name"))
 
 
 def test_modifgroup_header(app):
-    app.group.modify_first_group(Group(header="hhh41"))
-
+    if app.group.count() == 0:
+        app.group.create(Group(name="ЙЙЙ", header="ЙЙЙ"))
+    app.group.modify_first_group(Group(header="hhh10!header"))
