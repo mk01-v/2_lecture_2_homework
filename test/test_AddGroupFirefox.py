86a78bc041ed173a96bdfcd60dc676f9706a7aa4
mk01-v <prizma5150@gmail.com>
2019-11-15 12:37:22 +0700

    Статус: Работают все функции. Интеллектуалый логин работает. Редактирование группы, если данные заполнены/не заполнены.

--- test/test_AddGroupFirefox.py
+++ test/test_AddGroupFirefox.py
@@ -4,7 +4,7 @@ from model.group import Group
 
 def test_add_group(app):
     app.group.create(Group(name="group_name_cool", header="group_header_cool_logo"))
-    #app.session.logout()
+    app.session.logout()
 
 def test_add_empty_group(app):
     app.group.create(Group(name="", header=""))
--- test/test_amodifygroup.py
+++ test/test_amodifygroup.py
@@ -3,11 +3,9 @@ from model.group import Group
 
 
 def test_modifgroup_name(app):
-
     app.group.modify_first_group(Group(name="hhh1"))
 
 
 def test_modifgroup_header(app):
-
     app.group.modify_first_group(Group(header="hhh41"))
 
