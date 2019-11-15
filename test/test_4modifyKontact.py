c0d659b790a9edb469770a6565bdc11bad8605ed
mk01-v <prizma5150@gmail.com>
2019-11-15 16:44:10 +0700

    Изменение наименования.

--- test/test_4modifyKontact.py
+++ test/test_4modifyKontact.py
@@ -1,6 +1,6 @@
 from model.kontakt import kontakt
 
-def test_add_modif_kontakt(app):
+def test_modif_kontakt(app):
     app.kontakt.modif_kontakt(kontakt(username="Artur123123", middle_name="Middle_name: 1230000", last_name="", nickname="",
                             title="", company="", address="", home="",
                             mobile="", work="", fax="", email="",
