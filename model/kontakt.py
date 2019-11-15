0a4ec436cbea89b14291c2a5c5a193bf173f41e4
mk01-v <prizma5150@gmail.com>
2019-11-15 16:43:29 +0700

    Добавление None.

--- model/kontakt.py
+++ model/kontakt.py
@@ -1,9 +1,9 @@
 
 class kontakt:
 
-    def __init__(self, username, middle_name, last_name, nickname, title, company, address, home,
-                 mobile, work, fax, email, email2, email3, homepage, bday, bmonth, byear, aday,
-                 amonth, ayear, secondary_address2, secondary_home2, secondary_notes):
+    def __init__(self, username=None, middle_name=None, last_name=None, nickname=None, title=None, company=None, address=None, home=None,
+                 mobile=None, work=None, fax=None, email=None, email2=None, email3=None, homepage=None, bday=None, bmonth=None, byear=None, aday=None,
+                 amonth=None, ayear=None, secondary_address2=None, secondary_home2=None, secondary_notes=None):
         self.username = username
         self.middle_name = middle_name
         self.last_name = last_name
