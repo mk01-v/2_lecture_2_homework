76742ddbe0c1699baf9245193a51d99a0f6aa5ee
mk01-v <prizma5150@gmail.com>
2019-11-18 17:13:55 +0700

    Описание для class group

--- model/group.py
+++ model/group.py
@@ -5,3 +5,12 @@ class Group:
         self.name = name
         self.header = header
         self.id = id
+
+    #вывод содержимое объектов, а не адресов памяти.
+    def __repr__(self):
+        return "%s:%s" % (self.id, self.name)
+
+    # чтобы не сравнивал по физическому расположению, а сравнивал логически - индификаторы и имена.
+    # необходимо для сравнения самих объектов.
+    def __eq__(self, other):
+        return self.id == other.id and self.name == other.name
