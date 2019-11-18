2246547c4f65edf5682ca9127b03b45f4a927c4d
mk01-v <prizma5150@gmail.com>
2019-11-18 18:12:03 +0700

    Статус: работает. Добавлен тест для модификации.

--- test/test_3modifygroup.py
+++ test/test_3modifygroup.py
@@ -16,3 +16,13 @@ def test_modifgroup_header(app):
     app.group.modify_first_group(Group(header="hhh10!header"))
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
+
+def test_modifgroup_name2(app):
+    old_groups = app.group.get_group_list()
+    group = Group(name="New_group")
+    group.id = old_groups[0].id
+    app.group.modify_first_group(group)
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
