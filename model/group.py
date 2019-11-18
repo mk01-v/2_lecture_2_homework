f5ad856619e4f04d065239bb783d38835eb6e8a9
mk01-v <prizma5150@gmail.com>
2019-11-18 16:32:26 +0700

    для подсчета списков заведен параметр id

--- model/group.py
+++ model/group.py
@@ -1,6 +1,7 @@
 
 class Group:
 
-    def __init__(self, name=None, header=None):
+    def __init__(self, name=None, header=None, id=None):
         self.name = name
         self.header = header
+        self.id = id
