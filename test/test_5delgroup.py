a17f3da48e57a6787b279ca35ea85eb82e997a85
mk01-v <prizma5150@gmail.com>
2019-11-18 17:14:48 +0700

    Статус: работает. Добавлена еще проверка.

--- test/test_5delgroup.py
+++ test/test_5delgroup.py
@@ -15,3 +15,6 @@ def test_delete_first_group2(app):
     app.group.delete_first_group()
     new_groups = app.group.get_group_list()
     assert len(old_groups) - 1 == len(new_groups)
+    #Удаление 1 элемента, 2 не включается.
+    old_groups[0:1] = []
+    assert old_groups == new_groups
