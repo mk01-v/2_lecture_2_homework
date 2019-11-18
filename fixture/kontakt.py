28e0a36eb6e04302bd44f77f17e864c72efeb40e
mk01-v <prizma5150@gmail.com>
2019-11-18 10:18:21 +0700

    Статус: работает. Добавлена оптимизация страниц (open_kontakt).

--- fixture/kontakt.py
+++ fixture/kontakt.py
@@ -8,7 +8,9 @@ class KontaktHelper:
     def open_kontakt_page(self):
         # open kontakt page
         wd = self.app.wd
-        wd.find_element_by_link_text("add new").click()
+        if not (wd.current_url.endswith("/edit.php") and len(wd.find_elements_by_name("firstname")) > 0):
+            wd.find_element_by_link_text("add new").click()
+        #wd.find_element_by_link_text("add new").click()
 
     def create_kontakt(self, kontakt):
         wd = self.app.wd
