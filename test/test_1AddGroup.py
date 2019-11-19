d3e04e3f83a1573105b84853252cfeb240c80cb0
mk01-v <prizma5150@gmail.com>
2019-11-19 10:53:09 +0700

    Статус: работает. Имитирование хэширования.

--- test/test_1AddGroup.py
+++ test/test_1AddGroup.py
@@ -6,10 +6,10 @@ def test_add_group(app):
     old_groups = app.group.get_group_list()
     group = Group(name="group_name_cool", header="group_header_cool_logo")
     app.group.create(group)
+    
+    #Проверки со слова assert. Условие выполнилось, т.к. создалась 1 группа. Сравниваем длину 2-х списков.
+    assert len(old_groups) + 1 == app.group.count()
     new_groups = app.group.get_group_list()
-
-    #Проверки со слова assert. Условие выполнилось, т.к. создалась 1 группа.
-    assert len(old_groups) + 1 == len(new_groups)
     old_groups.append(group)
 
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
@@ -17,11 +17,11 @@ def test_add_group(app):
     # Проверка что остаемся в сессии.
     app.session.logout()
 
-def test_add_empty_group(app):
-    old_groups = app.group.get_group_list()
-    group = Group(name="", header="")
-    app.group.create(group)
-    new_groups = app.group.get_group_list()
-    assert len(old_groups) + 1 == len(new_groups)
-    old_groups.append(group)
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+#def test_add_empty_group(app):
+#    old_groups = app.group.get_group_list()
+#    group = Group(name="", header="")
+#    app.group.create(group)
+#    new_groups = app.group.get_group_list()
+#    assert len(old_groups) + 1 == len(new_groups)
+#    old_groups.append(group)
+#    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
