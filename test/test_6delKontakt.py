f5135496fc2cd14973c35af53e6fb40ff04e69c2
mk01-v <prizma5150@gmail.com>
2019-11-20 11:30:24 +0700

    Статус: работает. Удаление работает корректно со всеми параметрами.

--- test/test_6delKontakt.py
+++ test/test_6delKontakt.py
@@ -1,4 +1,5 @@
 from model.kontakt import kontakt
+from random import randrange
 
 def test_delete_kontakt(app):
     if app.kontakt.count_kontakt() == 0:
@@ -30,3 +31,44 @@ def test_delete_kontakt(app):
                                            secondary_home2="777",
                                            secondary_notes="888"))
     app.kontakt.delete_kontakt()
+
+def test_delete_kontakt_list(app):
+    if app.kontakt.count_kontakt() == 0:
+        app.kontakt.create_kontakt(kontakt(#username="11",
+                                           #middle_name="22",
+                                           last_name="33"
+                                           #nickname="44",
+                                           #title="55",
+                                           #company="66",
+                                           #address="77",
+                                           #home="88",
+                                           #mobile="99",
+                                           #work="00",
+                                           #fax="111",
+                                           #email="222",
+                                           #email2="333",
+                                           #email3="444",
+                                           #homepage="555",
+
+                                           #bday="16",
+                                           #bmonth="July",
+                                           #byear="2008",
+
+                                           #aday="16",
+                                           #amonth="January",
+                                           #ayear="2010",
+
+                                           #secondary_address2="666",
+                                           #secondary_home2="777",
+                                           #secondary_notes="888"
+        ))
+    old_kontakts = app.kontakt.get_kontakt_list()
+    index = randrange(len(old_kontakts))
+    app.kontakt.delete_kontakt_by_index(index)
+    new_kontakts = app.kontakt.get_kontakt_list()
+    assert len(old_kontakts) - 1 == len(new_kontakts)
+    old_kontakts[index:index+1] = []
+    assert old_kontakts == new_kontakts
+
+
+
