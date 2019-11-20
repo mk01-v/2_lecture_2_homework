9f15f7ccbcc1f59f346f4333c41859809eb56fc0
mk01-v <prizma5150@gmail.com>
2019-11-20 15:19:01 +0700

    Списки для обучения.

---
+++ try_to_prog.py
@@ -0,0 +1,23 @@
+
+#s = [10, 40, 20, 30]
+#print(s)
+#for element in range(len(s)):
+#    s[element] = s[element] + 2
+#print(s)
+
+l1 = [[['Andrey'],['Petrov']],[['Atrem'],['Sidorov']], [['Maksim'], ['Kukushkin']]]
+print(l1[0][0])
+print(l1[0][1])
+print(l1[1][0])
+print(l1[1][1])
+print(l1[2][0])
+print(l1[2][1])
+
+l2 = [['Andrey','Petrov'],['Atrem','Sidorov'], ['Maksim', 'Kukushkin']]
+
+print(l2[0][0])
+print(l2[0][1])
+print(l2[1][0])
+print(l2[1][1])
+print(l2[2][0])
+print(l2[2][1])
