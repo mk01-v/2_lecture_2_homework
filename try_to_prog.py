56febba3e276c972d600988e6e8cf8d400ef48a7
mk01-v <prizma5150@gmail.com>
2019-11-20 16:03:07 +0700

    5 занятие, лек 1

--- try_to_prog.py
+++ try_to_prog.py
@@ -5,19 +5,47 @@
 #    s[element] = s[element] + 2
 #print(s)
 
-l1 = [[['Andrey'],['Petrov']],[['Atrem'],['Sidorov']], [['Maksim'], ['Kukushkin']]]
-print(l1[0][0])
-print(l1[0][1])
-print(l1[1][0])
-print(l1[1][1])
-print(l1[2][0])
-print(l1[2][1])
 
-l2 = [['Andrey','Petrov'],['Atrem','Sidorov'], ['Maksim', 'Kukushkin']]
+#l1 = [[['Andrey'],['Petrov']],[['Atrem'],['Sidorov']], [['Maksim'], ['Kukushkin']]]
+#print(l1[0][0])
+#print(l1[0][1])
+#rint(l1[1][0])
+#print(l1[1][1])
+#print(l1[2][0])
+#print(l1[2][1])
+#l2 = [['Andrey','Petrov'],['Atrem','Sidorov'], ['Maksim', 'Kukushkin']]
+#print(l2[0][0])
+#print(l2[0][1])
+#print(l2[1][0])
+#print(l2[1][1])
+#print(l2[2][0])
+#print(l2[2][1])
 
-print(l2[0][0])
-print(l2[0][1])
-print(l2[1][0])
-print(l2[1][1])
-print(l2[2][0])
-print(l2[2][1])
+s1 = 'test\ntest'
+#print(s1)
+s2 = '''test
+test'''
+#print(s2)
+print(s1[1])
+print(s1[-1])
+#вырезка
+print(s1[0:2])
+s3 = 'test'
+#часть текста начинается с 'te'. возвращает True.
+print(s3.startswith("te"))
+print(s3.endswith("te"))
+# индекс расположения буквы. Начинается отсчет с 0.
+print(s3.index('s'))
+
+#поиск, возращает индекс символа или нескольких символов. При неудаче - "-1".
+print(s3.find('e'))
+print(s3.find('set'))
+
+#соединяет в 1 список.
+s4 = ('testwett\nesttex')
+#kkk = s4.split('\n')
+#print(kkk)
+
+#соединяет в 1 элемент, разделяя пробелами.
+print(' '.join(s1))
+
