2724817bca11d72ba5c485696ccf711685008134
mk01-v <prizma5150@gmail.com>
2019-11-28 18:19:32 +0700

    ---

---
+++ 5_lecture_3_work.py
@@ -0,0 +1,45 @@
+
+import random
+# импорт констант содержащих символов
+import string
+
+# вывод случайных символов.
+#print(random.choice('abcd'))
+
+# вывод всех символов
+#print(string.ascii_letters)
+#print(string.digits)
+
+# генератор случайной буквы или числа.
+#print(random.choice(string.ascii_letters + string.digits))
+
+# генератор с помощью вывернутого цикла фор
+# не работает
+#print(random.choice(string.ascii_letters + string.digits) for i in range(20))
+# работает
+#for i in range(20):
+#    print(random.choice(string.ascii_letters + string.digits))
+
+# не работает склейка в 1 строку.
+x = []
+for i in range(5):
+    x[i] = x[i] + i
+#''.join(random.choice(string.ascii_letters + string.digits))
+print(x)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
