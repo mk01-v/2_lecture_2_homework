6be9973495a4afd48f8a9a5c39f86b756a4e404d
mk01-v <prizma5150@gmail.com>
2019-11-29 17:07:39 +0700

    Статус: работает. Методы под индекс для сравнения тел. номеров Переделан get_kontakt_list добавлен поиск по username и last_name.

--- fixture/kontakt.py
+++ fixture/kontakt.py
@@ -31,6 +31,7 @@ class KontaktHelper:
         self.change_field_value_kontakt("title", kontakt.title)
         self.change_field_value_kontakt("company", kontakt.company)
         self.change_field_value_kontakt("address", kontakt.address)
+
         self.change_field_value_kontakt("home", kontakt.home)
         self.change_field_value_kontakt("mobile", kontakt.mobile)
         self.change_field_value_kontakt("work", kontakt.work)
@@ -81,7 +82,7 @@ class KontaktHelper:
 
     def modif_kontakt_by_index(self, index, kontakt):
         wd = self.app.wd
-        wd.find_element_by_link_text("home").click()
+        self.open_home_kontakt_page()
         self.select_kontakt_by_index(index)
         wd.find_element_by_xpath("//img[@alt='Edit']").click()
         self.fill_kontakt_form(kontakt)
@@ -90,7 +91,7 @@ class KontaktHelper:
 
     def select_kontakt_by_index(self, index):
         wd = self.app.wd
-        wd.find_element_by_link_text("home").click()
+        self.open_home_kontakt_page()
         wd.find_elements_by_name("selected[]")[index].click()
 
     def delete_kontakt(self):
@@ -98,7 +99,7 @@ class KontaktHelper:
 
     def delete_kontakt_by_index(self, index):
         wd = self.app.wd
-        wd.find_element_by_link_text("home").click()
+        self.open_home_kontakt_page()
         self.select_kontakt_by_index(index)
         # submit deletion
         wd.find_element_by_xpath("//input[@value='Delete']").click()
@@ -110,13 +111,17 @@ class KontaktHelper:
     def return_to_main_home(self):
         wd = self.app.wd
         # return groups page
-        wd.find_element_by_link_text("home").click()
+        self.open_home_kontakt_page()
 
     # подсчет количества контактов. Необходимость: проверка перед выполнением теста - создавать или нет предварительно контакт.
     def count_kontakt(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("home").click()
+        self.open_home_kontakt_page()
         return len(wd.find_elements_by_name("selected[]"))
+
+    def open_home_kontakt_page(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("home").click()
 
     kontakt_cache = None
 
@@ -124,16 +129,84 @@ class KontaktHelper:
         # делаем проверку.
         if self.kontakt_cache is None:
             wd = self.app.wd
-            wd.find_element_by_link_text("home").click()
+            self.open_home_kontakt_page()
             self.kontakt_cache = []
             # Проверка по наименованию групп.
             # Проверить можно в браузере что выберется. Ввести в консоли f12 $$('span.group').
-            for element in wd.find_elements_by_css_selector('tr[name="entry"]'):
-                # get_text() - вызывает метод, а нужно обращаться к свойству текст: text.
-                text = element.text
-                id = element.find_element_by_name("selected[]").get_attribute("value")
-                self.kontakt_cache.append(kontakt(last_name=text, id=id))
+            for row in wd.find_elements_by_name("entry"):
+                cells = row.find_elements_by_tag_name("td")
+                username = cells[1].text
+                lastname = cells[2].text
+                id = cells[0].find_element_by_tag_name("input").get_attribute("value")
+                all_phones = cells[5].text.splitlines()
+                self.kontakt_cache.append(kontakt(username=username,last_name=lastname, id=id,
+                                                  home=all_phones[0],
+                                                  mobile=all_phones[1],
+                                                  work=all_phones[2],
+                                                  secondary_home2=all_phones[3]))
+        return list(self.kontakt_cache)
+
+            #for element in wd.find_elements_by_css_selector('tr[name="entry"]'):
+            #    # get_text() - вызывает метод, а нужно обращаться к свойству текст: text.
+            #    text = element.text
+            #    id = element.find_element_by_name("selected[]").get_attribute("value")
+            #    self.kontakt_cache.append(kontakt(last_name=text, id=id))
             # возращаем копию кэша.
-            return list(self.kontakt_cache)
+            #return list(self.kontakt_cache)
 
         #$$('tr[name="entry"]')
+
+    def open_kontakt_to_edit_by_index(self, index):
+        wd = self.app.wd
+        self.open_home_kontakt_page()
+        row = wd.find_elements_by_name("entry")[index]
+        cell = row.find_elements_by_tag_name('td')[7]
+        cell.find_element_by_tag_name("a").click()
+
+    def open_kontakt_view_by_index(self, index):
+        wd = self.app.wd
+        self.open_home_kontakt_page()
+        row = wd.find_elements_by_name("entry")[index]
+        cell = row.find_elements_by_tag_name('td')[6]
+        cell.find_element_by_tag_name("a").click()
+
+    def get_kontakt_info_from_edit_page(self, index):
+        wd = self.app.wd
+        self.open_kontakt_to_edit_by_index(index)
+
+        username = wd.find_element_by_name("firstname").get_attribute("value")
+        lastname = wd.find_element_by_name("lastname").get_attribute("value")
+        id = wd.find_element_by_name("id").get_attribute("value")
+        home = wd.find_element_by_name("home").get_attribute("value")
+        mobile = wd.find_element_by_name("mobile").get_attribute("value")
+        work = wd.find_element_by_name("work").get_attribute("value")
+        secondary_home2 = wd.find_element_by_name("phone2").get_attribute("value")
+
+        return kontakt(username=username,
+                       last_name=lastname,
+                       id=id,
+                       home=home,
+                       mobile=mobile,
+                       work=work,
+                       secondary_home2=secondary_home2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
