935f0913828a313cb6e3ac4b0aaea7981f35d4bc
mk01-v <prizma5150@gmail.com>
2019-11-29 12:29:08 +0700

    Добавление чатаемости кода. Улучшений/ускарений нет после этих изменений

--- fixture/session.py
+++ fixture/session.py
@@ -31,7 +31,12 @@ class SessionHelper:
 
     def is_logged_in_as(self, username):
         wd = self.app.wd
-        return wd.find_element_by_xpath("(.//*[normalize-space(text()) and normalize-space(.)='Address Book'])[1]/preceding::b[1]").text == "("+username+")"
+        return self.get_logged_user() == username
+
+    def get_logged_user(self):
+        wd = self.app.wd
+        # вырезка для логина. Обрезаем 1 символ и предпоследний - скобки.
+        return wd.find_element_by_xpath("(.//*[normalize-space(text()) and normalize-space(.)='Address Book'])[1]/preceding::b[1]").text[1:-1]
 
     def ensure_logout(self):
         wd = self.app.wd
