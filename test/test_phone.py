34f6f6769f023cad02e34b813517aa28bf0afdf7
mk01-v <prizma5150@gmail.com>
2019-12-02 09:29:52 +0700

    Статус: работает. Добавлена функция очистки от лишних символов.

--- test/test_phone.py
+++ test/test_phone.py
@@ -1,12 +1,18 @@
-
+import re
 
 def test_phones_on_home_page(app):
     kontakt_from_home_page = app.kontakt.get_kontakt_list()[0]
     kontakt_from_edit_page = app.kontakt.get_kontakt_info_from_edit_page(0)
-    assert kontakt_from_home_page.home == kontakt_from_edit_page.home
-    assert kontakt_from_home_page.mobile == kontakt_from_edit_page.mobile
-    assert kontakt_from_home_page.work == kontakt_from_edit_page.work
-    assert kontakt_from_home_page.fax == kontakt_from_edit_page.fax
+    assert kontakt_from_home_page.home == clear(kontakt_from_edit_page.home)
+    assert kontakt_from_home_page.mobile == clear(kontakt_from_edit_page.mobile)
+    assert kontakt_from_home_page.work == clear(kontakt_from_edit_page.work)
+    assert kontakt_from_home_page.secondary_home2 == clear(kontakt_from_edit_page.secondary_home2)
+
+    # clear, данные из формы редактирования нужно удалить, будем убирать лишние символы.
+
+def clear(s):
+    # 1 параметр что убираем, 2 на что, 3 параметр где заменять.
+    return re.sub("[() -]", "", s)
 
 
 
