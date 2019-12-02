a991c55a1a41c2d39c3074ce0d39160f9a91488d
mk01-v <prizma5150@gmail.com>
2019-12-02 11:33:57 +0700

    Статус: работает новый тест - проверяем edit_page и view_page.

--- test/test_phone.py
+++ test/test_phone.py
@@ -8,8 +8,16 @@ def test_phones_on_home_page(app):
     assert kontakt_from_home_page.work == clear(kontakt_from_edit_page.work)
     assert kontakt_from_home_page.secondary_home2 == clear(kontakt_from_edit_page.secondary_home2)
 
-    # clear, данные из формы редактирования нужно удалить, будем убирать лишние символы.
+def test_phone_on_contact_view_page(app):
+    kontakt_from_view_page = app.kontakt.get_kontakt_from_view_page(0)
+    kontakt_from_edit_page = app.kontakt.get_kontakt_info_from_edit_page(0)
+    assert kontakt_from_view_page.home == kontakt_from_edit_page.home
+    assert kontakt_from_view_page.mobile == kontakt_from_edit_page.mobile
+    assert kontakt_from_view_page.work == kontakt_from_edit_page.work
+    assert kontakt_from_view_page.secondary_home2 == kontakt_from_edit_page.secondary_home2
 
+
+    # clear, данные из формы редактирования нужно удалить, будем убирать лишние символы.
 def clear(s):
     # 1 параметр что убираем, 2 на что, 3 параметр где заменять.
     return re.sub("[() -]", "", s)
