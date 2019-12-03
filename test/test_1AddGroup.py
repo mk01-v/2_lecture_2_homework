db56b7e7469db26668164494d7463a437b7a81bf
mk01-v <prizma5150@gmail.com>
2019-12-03 18:26:58 +0700

    Статус: работает. Некорректно собирает список с пробелами, убрано из генератора.

--- test/test_1AddGroup.py
+++ test/test_1AddGroup.py
@@ -6,7 +6,7 @@ import string
 
 # генератор данных.
 def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + " "*10
+    symbols = string.ascii_letters + string.digits # + " "*10
     return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
 #Вводимые данные
