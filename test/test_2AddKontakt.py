cb17947445322bcee6478d621b95f407ebb27fd5
mk01-v <prizma5150@gmail.com>
2019-12-05 12:37:16 +0700

    Статус: работает. Параметризованный тест.

--- test/test_2AddKontakt.py
+++ test/test_2AddKontakt.py
@@ -1,7 +1,10 @@
-from model.kontakt import kontakt
+from model.kontakt import Kontakt
+import pytest
+import random
+import string
 
 def test_add_kontakt_firefox(app):
-    app.kontakt.create_kontakt(kontakt(username="First_name: Artur",
+    app.kontakt.create_kontakt(Kontakt(username="First_name: Artur",
                                        middle_name="Middle_name: 123",
                                        last_name="Last_name: Piroshkov",
                                        nickname="Nickname: 123",
@@ -32,7 +35,7 @@ def test_add_kontakt_firefox(app):
 
 def test_add_kontakt_list(app):
     old_kontakts = app.kontakt.get_kontakt_list()
-    Kontakt = kontakt(#username="Artur",
+    Kontakt = Kontakt(#username="Artur",
                       #middle_name="123",
                       last_name="Piroshkov",
                       #nickname="123",
@@ -65,36 +68,38 @@ def test_add_kontakt_list(app):
     assert len(old_kontakts) + 1 == app.kontakt.count_kontakt()
     new_kontakts = app.kontakt.get_kontakt_list()
     old_kontakts.append(Kontakt)
-    assert sorted(old_kontakts, key=kontakt.id_or_max_kontakt) == sorted(new_kontakts, key=kontakt.id_or_max_kontakt)
+    assert sorted(old_kontakts, key=Kontakt.id_or_max_kontakt) == sorted(new_kontakts, key=Kontakt.id_or_max_kontakt)
     # Проверка что остаемся в сессии.
     app.session.logout()
 #--------------------------------------------------------------------------------------------
-import pytest
-import random
-import string
+
 # Задание № 15 - генерация.
 # генератор данных.
 def random_string(prefix, maxlen):
     symbols = string.ascii_letters + string.digits # + " "*10
     return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
-def random_day(d):
-    #numeral = random.randint(1, 28)
-    return d(random.randint(1, 28))
+def random_telephone(maxlen):
+    symbols = string.digits
+    return ([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
-def random_month(n):
+def random_day():
     #numeral = random.randint(1, 28)
-    return n(random.choice(['January','February','March','April', 'May', 'June', 'July',
+    return (random.randint(1,30))
+
+def random_month():
+    #numeral = random.randint(1, 28)
+    return (random.choice(['January','February','March','April', 'May', 'June', 'July',
                             'August', 'September', 'October', 'November', 'December']))
 
-def random_year(y):
+def random_year():
     #numeral = random.randint(1, 28)
-    return y(random.choice(['January','February','March','April', 'May', 'June', 'July',
+    return (random.choice(['January','February','March','April', 'May', 'June', 'July',
                             'August', 'September', 'October', 'November', 'December']))
 
 
 #Вводимые данные
-testdata = [kontakt(username="",
+testdata = [Kontakt(username="",
                     middle_name="",
                     last_name="",
                     nickname="",
@@ -110,63 +115,64 @@ testdata = [kontakt(username="",
                     email3="",
                     homepage="",
 
-                    bday="",
-                    bmonth="",
-                    byear="",
+                    #bday="",
+                    #bmonth="",
+                    #byear="",
 
-                    aday="",
-                    amonth="",
-                    ayear="",
+                    #aday="",
+                    #amonth="",
+                    #ayear="",
 
                     secondary_address2="",
                     secondary_home2="",
                     secondary_notes=""
-)] + [
-    kontakt(        username=random_string("username", 10),
-                    middle_name=random_string("middlename", 10),
-                    last_name=random_string("lastname", 10),
-                    nickname=random_string("nickname", 10),
-                    title=random_string("title", 10),
-                    company=random_string("company", 10),
-                    address=random_string("address", 10),
-                    home=random_string("home", 10),
-                    mobile=random_string("mobile", 10),
-                    work=random_string("work", 10),
-                    fax=random_string("fax", 10),
-                    email=random_string("email", 10),
-                    email2=random_string("email2", 10),
-                    email3=random_string("email3", 10),
-                    homepage=random_string("homepage", 10),
+                    )] + [
+    Kontakt(username=random_string("username", 10),
+            middle_name=random_string("middlename", 10),
+            last_name=random_string("lastname", 10),
+            nickname=random_string("nickname", 10),
+            title=random_string("title", 10),
+            company=random_string("company", 10),
+            address=random_string("address", 10),
+            home=random_telephone(10),
+            mobile=random_telephone(10),
+            work=random_telephone(10),
+            fax=random_telephone(10),
+            email=random_string("email", 10),
+            email2=random_string("email2", 10),
+            email3=random_string("email3", 10),
+            homepage=random_string("homepage", 10),
 
-                    bday=,
-                    bmonth=,
-                    byear=,
+            #bday=random_day(),
+            #bmonth=random_month(),
+            #byear=random_year(),
 
-                    aday=,
-                    amonth=,
-                    ayear=,
+            #aday=random_day(),
+            #amonth=random_month(),
+            #ayear=random_year(),
 
-                    secondary_address2=random_string("address2", 10),
-                    secondary_home2=random_string("phone2", 10),
-                    secondary_notes=random_string("notes", 10))
-    for i in range(5)
+            secondary_address2=random_string("address2", 10),
+            secondary_home2=random_telephone(10),
+            secondary_notes=random_string("notes", 10))
+    for i in range(1)
 ]
 
-@pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
-def test_add_group(app, group):
+@pytest.mark.parametrize("kontaktpr", testdata, ids=[repr(x) for x in testdata])
+def test_add_kontakt_gen(app, kontaktpr):
     #pass
     # добавляем тестовые данные
     # добаляем цикл данных
     #объявляем старый список групп.
-    old_groups = app.group.get_group_list()
+    old_kontakt = app.kontakt.get_kontakt_list()
     # теперь строка загрузки данных не нужна.
     #group = Group(name="group_name_cool", header="group_header_cool_logo")
-    app.group.create(group)
+    app.kontakt.create_kontakt(kontaktpr)
     #Проверки со слова assert. Условие выполнилось, т.к. создалась 1 группа. Сравниваем длину 2-х списков.
-    assert len(old_groups) + 1 == app.group.count()
-    new_groups = app.group.get_group_list()
-    old_groups.append(group)
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+    assert len(old_kontakt) + 1 == app.kontakt.count_kontakt()
+    new_kontakt = app.kontakt.get_kontakt_list()
+    old_kontakt.append(kontaktpr)
+    #assert sorted(old_kontakt) == sorted(new_kontakt)
+    assert sorted(old_kontakt, key=Kontakt.id_or_max_kontakt) == sorted(new_kontakt, key=Kontakt.id_or_max_kontakt)
     # Проверка что остаемся в сессии.
     #app.session.logout()
 
