d475b9c5ebc8fbfa2d00394f347e0205dfb566f7
mk01-v <prizma5150@gmail.com>
2019-12-16 15:12:41 +0700

    ----

--- 6_lecture_1_work.py
+++ 6_lecture_1_work.py
@@ -1,4 +1,42 @@
 
+l = ['windows', '8.1', 'firefox', '35.0.1']
+print(l)
+print(l[0])
+
+t = ('windows', '8.1', 'firefox', '35.0.1')
+print(t[0])
+
+# список объектов с метками или ключами. Так удобнее.
+d = {'os': 'windows', 'os_version': '8.1', 'browser':'firefox', 'browser_version':'35.0.1'}
+print(d['os'])
+print(d['browser'])
+
+# вложенные словари.
+k = {'os': {'type':'windows', 'version':'8.1'}, 'browser': {'type':'firefox', 'version':'35.0.1'}}
+print(k['browser']['version'])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
