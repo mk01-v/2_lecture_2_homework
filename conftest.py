93d6b2adb148113e93d28f7132a873c9eaf09b6e
mk01-v <prizma5150@gmail.com>
2019-12-20 11:00:45 +0700

    модернизируем conftest

--- conftest.py
+++ conftest.py
@@ -1,5 +1,6 @@
 from fixture.application import Application
 import pytest
+import json
 
 
 #Для быстрого запуска - в рамках одной сессии.
@@ -23,13 +24,11 @@ fixture = None
 def app(request):
     global fixture
     browser = request.config.getoption("--browser")
-    base_Url = request.config.getoption("--baseUrl")
-    if fixture is None:
-        fixture = Application(browser=browser, base_Url=base_Url)
-    else:
-        if not fixture.is_valid():
-            fixture = Application(browser=browser, base_Url=base_Url)
-    fixture.session.ensure_login(username="admin", password="secret")
+    with open(request.config.getoption("--target")) as config_file:
+        target = json.load(config_file)
+    if fixture is None or not fixture.is_valid:
+        fixture = Application(browser=browser, base_Url=target['baseUrl'])
+    fixture.session.ensure_login(username=target["username"], password=target["password"])
     return fixture
 
 # срабатывание всегда из-за autouse.
@@ -44,4 +43,4 @@ def stop(request):
 # Зацепка (hook). Добавляет доп. параметр. 1 параметр добавляем браузер, 2 действие - сохранить, 3 по умолчанию.
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="Firefox")
-    parser.addoption("--baseUrl", action="store", default="http://localhost/addressbook/")
+    parser.addoption("--target", action="store", default="target.json")
