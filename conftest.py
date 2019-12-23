fb9be6a85ea8c5fef1bcbac09c5c8a4b36217975
mk01-v <prizma5150@gmail.com>
2019-12-23 17:16:41 +0700

    Занятие 2 подготовка к работе с базой данных, запусков/провеок не было.

--- conftest.py
+++ conftest.py
@@ -4,6 +4,7 @@ import json
 import os.path
 import importlib
 import jsonpickle
+from fixture.db import DbFixture
 
 
 #Для быстрого запуска - в рамках одной сессии.
@@ -24,11 +25,8 @@ import jsonpickle
 fixture = None
 target = None
 
-@pytest.fixture
-def app(request):
-    global fixture
+def load_config(file):
     global target
-    browser = request.config.getoption("--browser")
     # не забыть указать корень директории проекта, иначе не будет работать target.json
     # *запуск проектов* - edit configuration - working directory.
     # C:\Python\Projects\2_lecture_2_homework
@@ -36,12 +34,20 @@ def app(request):
         # прописывание директории по-умолчанию, т.к. не цеплялся файл json к проекту.
         # 1 - abspath преоброзование в абсолютный путь, 2 dirname получили директорию, 3 - к этой директории приклеиваем ..
         # .. путь к конфигурационному файлу join. 4 - то что собираемся подклеить request.config.getoption("--target").
-        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption("--target"))
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), file)
         with open(config_file) as f:
             target = json.load(f)
+    return target
+
+
+@pytest.fixture
+def app(request):
+    global fixture
+    browser = request.config.getoption("--browser")
+    web_config = load_config(request.config.getoption("--target"))['web']
     if fixture is None or not fixture.is_valid:
-        fixture = Application(browser=browser, base_Url=target['baseUrl'])
-    fixture.session.ensure_login(username=target["username"], password=target["password"])
+        fixture = Application(browser=browser, base_Url=web_config['baseUrl'])
+    fixture.session.ensure_login(username=web_config["username"], password=web_config["password"])
     return fixture
 
 # срабатывание всегда из-за autouse.
@@ -69,7 +75,6 @@ def pytest_generate_tests(metafunc):
             testdata = load_from_json(fixture[5:])
             metafunc.parametrize(fixture, testdata, ids=[str(x) for x in testdata])
 
-
 def load_from_module(module):
     return importlib.import_module("data.%s" % module).testdata
 
@@ -77,6 +82,14 @@ def load_from_json(file):
     with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "data/%s.json" % file)) as f:
         return jsonpickle.decode(f.read())
 
+@pytest.fixture(scope="session")
+def db(request):
+    db_config = load_config(request.config.getoption("--target"))['db']
+    dbfixture = DbFixture(host=db_config['host'], name=db_config['name'], user=db_config['user'], password=db_config['password'])
+    def fin():
+        dbfixture.destroy()
+        request.addfinalizer(fin)
+    return dbfixture
 
 
 
