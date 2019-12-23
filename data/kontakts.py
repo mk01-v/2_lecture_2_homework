cc6b4e243646c3cd9ee9d98be23ad2dcf5eb802c
mk01-v <prizma5150@gmail.com>
2019-12-23 13:52:59 +0700

    Вспомогательный файл для проверок

---
+++ data/kontakts.py
@@ -0,0 +1,80 @@
+from model.kontakt import Kontakt
+
+testdata = [Kontakt(username="",
+                    middle_name="",
+                    last_name="",
+                    nickname="",
+                    title="",
+                    company="",
+                    address="",
+                    home="",
+                    mobile="",
+                    work="",
+                    fax="",
+                    email="",
+                    email2="",
+                    email3="",
+                    homepage="",
+
+                    bday="-",
+                    bmonth="-",
+                    byear="-",
+
+                    aday="-",
+                    amonth="-",
+                    ayear="-",
+
+                    secondary_address2="",
+                    secondary_home2="",
+                    secondary_notes=""
+                    )] + [
+    Kontakt(username="username1",
+            middle_name="middle_name2",
+            last_name="last_name3",
+            nickname="nickname4",
+            #title=random_string("title", 10),
+            #company=random_string("company", 10),
+            #address=random_string("address", 10),
+            #home=random_telephone(10),
+            #mobile=random_telephone(10),
+            #work=random_telephone(10),
+            #fax=random_telephone(10),
+            #email=random_string("email", 10),
+            #email2=random_string("email2", 10),
+            #email3=random_string("email3", 10),
+            #homepage=random_string("homepage", 10),
+
+            #bday=random_day(),
+            #bmonth=random_month(),
+            #byear=random_year(),
+
+            #aday=random_day(),
+            #amonth=random_month(),
+            #ayear=random_year(),
+
+            #secondary_address2=random_string("address2", 10),
+            #secondary_home2=random_telephone(10),
+            #secondary_notes=random_string("notes", 10))
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
