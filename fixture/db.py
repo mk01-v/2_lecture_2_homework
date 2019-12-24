b1ff4e65a64830c683223782bb1838406e0bc9d4
mk01-v <prizma5150@gmail.com>
2019-12-24 13:45:09 +0700

    Выборка из БД id, firstname, lastname from addressbook где указаны текущие, а не все созданные.

--- fixture/db.py
+++ fixture/db.py
@@ -1,5 +1,6 @@
 import mysql.connector
 from model.group import Group
+from model.kontakt import Kontakt
 
 class DbFixture:
     # autocommit=True кэш после каждого запроса сбрасывается.
@@ -25,6 +26,20 @@ class DbFixture:
                 # в каком порядке выстроить параметры.
                 (id, name, header) = row
                 list.append(Group(id=str(id), name=name, header=header))
+        finally:
+            cursor.close()
+        return list
+
+    # Выборка из БД id, firstname, lastname from addressbook где указаны текущие, а не все созданные.
+    def get_kontakt_list_db(self):
+        list = []
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute("select id, firstname, lastname from addressbook where deprecated='0000-00-00 00:00:00'")
+            for row in cursor:
+                # в каком порядке выстроить параметры.
+                (id, firstname, lastname) = row
+                list.append(Kontakt(id=str(id), username=firstname, last_name=lastname))
         finally:
             cursor.close()
         return list
