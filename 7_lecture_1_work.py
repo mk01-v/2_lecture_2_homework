8d97f25d9b772b532acd08a5fc8772d7fee672a8
mk01-v <prizma5150@gmail.com>
2019-12-24 19:00:10 +0700

    Проверка контактов в группе.

--- 7_lecture_1_work.py
+++ 7_lecture_1_work.py
@@ -1,8 +1,9 @@
 from fixture.orm import ORMFixture
+from model.group import Group
 
 db = ORMFixture(host="127.0.0.1", name="addressbook", user="root", password="")
 try:
-    l = db.get_kontact_list()
+    l = db.get_kontakts_in_group(Group(id="63"))
     for item in l:
         print(item)
     print(len(l))
