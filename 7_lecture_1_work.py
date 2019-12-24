9fe07f5a630e9d72c2cedda1a294b3baec73982d
mk01-v <prizma5150@gmail.com>
2019-12-24 13:45:53 +0700

    # проверка загрузки данных контактов.

--- 7_lecture_1_work.py
+++ 7_lecture_1_work.py
@@ -1,18 +1,31 @@
+from fixture.db import DbFixture
+
+db = DbFixture(host="127.0.0.1", name="addressbook", user="root", password="")
+# проверка загрузки данных контактов.
+try:
+    kontakts = db.get_kontakt_list_db()
+    for kontakt in kontakts:
+        print(kontakt)
+    print(len(kontakts))
+finally:
+    db.destroy()
+
+
+#--------------------------------------
 # Проверка подключения БД
 # для коннектора 8.0.18
 #import mysql.connector
 
-# для pymysql лучше использовать этот отмечает Баранцев.
-import pymysql.cursors
+# для sql лучше использовать этот драйвер отмечает Баранцев.
+#import pymysql.cursors
 
-connection = pymysql.connect(host="127.0.0.1", database="addressbook", user="root", password="")
+#connection = pymysql.connect(host="127.0.0.1", database="addressbook", user="root", password="")
 # с коннектором 8.0.18 mysql.connector.connect(host="127.0.0.1", database="addressbook", user="root", password="")
-
-try:
-    # необходим курсор для указания данных.
-    cursor = connection.cursor()
-    cursor.execute("select * from group_list")
-    for row in cursor.fetchall():
-        print(row)
-finally:
-    connection.close()
+#try:
+#    # необходим курсор для указания данных.
+#    cursor = connection.cursor()
+#    cursor.execute("select * from group_list")
+#    for row in cursor.fetchall():
+#        print(row)
+#finally:
+#    connection.close()
