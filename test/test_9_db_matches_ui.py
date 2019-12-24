f3db20f909cc5d4783f8d53628f4c38888e8e669
mk01-v <prizma5150@gmail.com>
2019-12-24 11:42:28 +0700

    Проверка скорости загрузки через БД и UI.

--- test/test_9_db_matches_ui.py
+++ test/test_9_db_matches_ui.py
@@ -1,13 +1,16 @@
 from model.group import Group
-
+from timeit import timeit
 
 def test_group_list(app, db):
-    ui_list = app.group.get_group_list()
+    # время выполнения 2-ух операций
+    print(timeit(lambda: app.group.get_group_list(), number=1))
+    #ui_list = app.group.get_group_list()
     # удаление пробелов, временно для прохождения теста.
     def clean(group):
         return Group(id=group.id, name=group.name.strip())
-    db_list = map(clean, db.get_group_list_db())
-    assert sorted(ui_list, key=Group.id_or_max) == sorted(db_list, key=Group.id_or_max)
+    print(timeit(lambda: map(clean, db.get_group_list_db()), number=1000))
+    #db_list = map(clean, db.get_group_list_db())
+    assert False #sorted(ui_list, key=Group.id_or_max) == sorted(db_list, key=Group.id_or_max)
 
 
 
