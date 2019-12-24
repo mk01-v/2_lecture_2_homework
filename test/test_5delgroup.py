19e60c171817dd71f8aa59aaef58e10b3b6eafd6
mk01-v <prizma5150@gmail.com>
2019-12-24 11:57:15 +0700

    # отключаемая проверка.

--- test/test_5delgroup.py
+++ test/test_5delgroup.py
@@ -34,7 +34,7 @@ import random
 #    old_groups[index:index+1] = []
 #    assert old_groups == new_groups
 
-def test_delete_some_group(app, db):
+def test_delete_some_group(app, db, check_ui):
     if len(db.get_group_list_db()) == 0:
         app.group.create(Group(name="test", header="qweqwe"))
     old_groups = db.get_group_list_db()
@@ -43,6 +43,21 @@ def test_delete_some_group(app, db):
     app.group.delete_group_by_id(group.id)
     new_groups = db.get_group_list_db()
     assert len(old_groups) - 1 == len(new_groups)
-
     old_groups.remove(group)
     assert old_groups == new_groups
+    # edit configuration - additional arguments - --check_ui
+    # отключаемая проверка.
+    if check_ui:
+        assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
+
+
+
+
+
+
+
+
+
+
+
+
