8c80739912c9b8bd25b1796696ba217db68d9164
mk01-v <prizma5150@gmail.com>
2019-12-26 15:38:01 +0700

    Вывод групп в списке и не в списке.

--- 7_lecture_1_work.py
+++ 7_lecture_1_work.py
@@ -3,14 +3,15 @@ from model.group import Group
 
 db = ORMFixture(host="127.0.0.1", name="addressbook", user="root", password="")
 try:
-    l = db.get_kontakts_in_group(Group(id="63"))
+    # группы, в списке
+    #l = db.get_kontakts_in_group(Group(id="2"))
+    # группы, не в списке
+    l = db.get_kontakts_not_in_group(Group(id="2"))
     for item in l:
         print(item)
     print(len(l))
 finally:
     pass #db.destroy()
-
-
 
 
 
