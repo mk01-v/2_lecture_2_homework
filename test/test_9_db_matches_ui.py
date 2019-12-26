9c07abe809f5b155ed2aadb42fa26aebb02594ec
mk01-v <prizma5150@gmail.com>
2019-12-26 13:20:25 +0700

    Сверка списков из ui и БД.

--- test/test_9_db_matches_ui.py
+++ test/test_9_db_matches_ui.py
@@ -1,4 +1,5 @@
 from model.group import Group
+from model.kontakt import Kontakt
 from timeit import timeit
 
 def test_group_list(app, db):
@@ -12,7 +13,17 @@ def test_group_list(app, db):
     #db_list = map(clean, db.get_group_list_db())
     assert False #sorted(ui_list, key=Group.id_or_max) == sorted(db_list, key=Group.id_or_max)
 
-
+def test_kontakt_list(app, db):
+    # время выполнения 2-ух операций
+    ui_list = app.kontakt.get_kontakt_list()
+    # удаление пробелов, временно для прохождения теста.
+    # strip - удаление пробелов.
+    def clean(kontakt):
+        return Kontakt(id=kontakt.id, username=kontakt.username.strip(), last_name=kontakt.last_name.strip())
+    # работает с Clean и без.
+    db_list = map(clean, db.get_kontakt_list_db())
+    #db_list = db.get_kontakt_list_db()
+    assert sorted(ui_list, key=Group.id_or_max) == sorted(db_list, key=Group.id_or_max)
 
 
 
