8869c12e4bab2545b015a0f415d2a7b25b525ade
mk01-v <prizma5150@gmail.com>
2019-12-26 18:03:14 +0700

    ----

--- 7_lecture_1_work.py
+++ 7_lecture_1_work.py
@@ -4,9 +4,10 @@ from model.group import Group
 db = ORMFixture(host="127.0.0.1", name="addressbook", user="root", password="")
 try:
     # группы, в списке
-    #l = db.get_kontakts_in_group(Group(id="2"))
+    #k = db.g
+    l = db.get_kontakts_in_group(Group(id="2"))
     # группы, не в списке
-    l = db.get_kontakts_not_in_group(Group(id="2"))
+    #l = db.get_kontakts_not_in_group(Group(id="2"))
     for item in l:
         print(item)
     print(len(l))
