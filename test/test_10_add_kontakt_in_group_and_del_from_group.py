f523669cf9ac5a67f552caac003a7a62cd2730ce
mk01-v <prizma5150@gmail.com>
2019-12-26 18:02:30 +0700

    ---

--- test/test_10_add_kontakt_in_group_and_del_from_group.py
+++ test/test_10_add_kontakt_in_group_and_del_from_group.py
@@ -1,8 +1,11 @@
 # импорты
 import random
+from fixture.orm import ORMFixture
+from model.kontakt import Kontakt
+from model.group import Group
 
 
-def test_add_kontakt_in_group(app, db):
+def test_add_kontakt_in_group(app, db, orm):
     # план
     # получить список контактов
     old_kontakts = db.get_kontakt_list_db()
@@ -14,6 +17,12 @@ def test_add_kontakt_in_group(app, db):
     take_group = random.choice(old_groups)
     # поместить случайный выбранный контакт в группу
     app.kontakt.add_kontakt_in_group(take_kontakt.id, take_group.name)
-    # переход на группу
-    app.kontakt.go_to_group(take_group.name)
+    # переход на группу?
+    #app.kontakt.go_to_group(take_group.name)
     # assert с БД
+    id_from_take_group = orm.get_kontakts_in_group(Group(id=take_group.id))
+    assert take_kontakt.id == id_from_take_group
+
+
+
+
