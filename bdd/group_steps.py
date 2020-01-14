dc637ed746f4e6e7f128ff792d75de9619df546c
mk01-v <prizma5150@gmail.com>
2020-01-14 17:40:12 +0700

    scenario

--- bdd/group_steps.py
+++ bdd/group_steps.py
@@ -1,6 +1,8 @@
 
 from pytest_bdd import given, when, then
 from model.group import Group
+import time
+import random
 
 @given('a group list')
 def group_list(db):
@@ -13,6 +15,7 @@ def new_group(name, header):
 @when('I add the group to the list')
 def add_new_group(app, new_group):
     app.group.create(new_group)
+    time.sleep(1)
 
 @then('the new group list is equal to the old list with the added group')
 def verify_group_added(db, group_list, new_group):
@@ -20,6 +23,21 @@ def verify_group_added(db, group_list, new_group):
     new_groups = db.get_group_list_db()
     old_groups.append(new_group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
-    
 
+@given('a non-empty group list')
+def non_empty_group_list(db, app):
+    if len(db.get_group_list_db()) == 0:
+        app.group.create(Group(name="some_name"))
+    return db.get_group_list_db()
+
+@given('a random group from the list')
+def random_group(non_empty_group_list):
+    return random.choice(non_empty_group_list)
+
+@when('I delete the group from the list')
+def delete_group(app, random_group):
+    app.group.app.group.delete_group_by_index(random_group.id)
+
+@then('the new list is equal to the old list without the deleted group')
+    def verify_group_deleted(db, non_empty_group_list, random_group):
 
